93b079bd064112183a40140a48872fda9bdb214a
Piotr Lasota <lasota.piotr1@gmail.com>
2021-05-24 14:23:51 +0200

    feat: add publishable lib

---
+++ libs/shared/lib-publishable/src/lib/shared-lib-publishable.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import SharedLibPublishable from './shared-lib-publishable';
+
+describe('SharedLibPublishable', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<SharedLibPublishable />);
+    expect(baseElement).toBeTruthy();
+  });
+});
